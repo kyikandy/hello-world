9437d27ea565900df027d4e111d3488ae50ec198
Minkah <87985203+kyikandy@users.noreply.github.com>
2021-08-10 20:19:21 +0300

    Add files via upload
    Group assignment for exercise 4. I did up to question 18.

---
+++ Group Assignment.py
@@ -0,0 +1,23 @@
+print('Thirty' + ' Days' + ' Of' ' Python.')
+print('Coding' + ' For' + ' All.')
+company = 'Coding For All'
+print(company)
+print(len(company))
+print(company.upper())
+print(company.lower())
+print(company.title()) 
+print(company.swapcase()) 
+print(company[0:6])
+print(company.startswith('Coding'))
+print(company.replace('Coding', 'Python'))
+print(company.split())
+Social_media = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+print(Social_media.split())
+first_index = company[0]
+print(first_index)
+tenth_index = company[10]
+print(tenth_index) 
+text = 'Python For Everyone'
+words = text.split()
+print(words)
+print(words[0]+words[1]+words[2])
